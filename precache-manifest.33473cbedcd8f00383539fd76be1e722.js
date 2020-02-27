68473f6ffb082d20d9ef1f8c166b3df529de75cb
jishnu <psjishnu13@gmail.com>
2020-02-27 22:27:01 +0530

    Updates

---
+++ precache-manifest.33473cbedcd8f00383539fd76be1e722.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efba5891164717495575e73d539ff766",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5bcf1f4295011dc63c1a",
+    "url": "/my-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f44023531537ce3db92f",
+    "url": "/my-app/static/js/2.e0ef1adb.chunk.js"
+  },
+  {
+    "revision": "ab427a65ed80b935d451c180b71c0220",
+    "url": "/my-app/static/js/2.e0ef1adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bcf1f4295011dc63c1a",
+    "url": "/my-app/static/js/main.ae03a448.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
